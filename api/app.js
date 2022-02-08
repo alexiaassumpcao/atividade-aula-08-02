1f768a695adacdbba79478177cdd9140a3de3c1f
Alexia <alexiaassumpcaorp@gmail.com>
2022-02-08 20:30:33 -0300

    Adiciona resource-status

--- api/app.js
+++ api/app.js
@@ -5,6 +5,11 @@ var app = express();
 app.get('/', function(req, res) {
   res.send('hello world');
 });
+
+app.get('/resource-status', function(req, res) {
+    res.send('OK').status(200);
+  });
+
 const port = 3000
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
